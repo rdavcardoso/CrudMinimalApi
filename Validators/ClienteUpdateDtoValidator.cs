87faef0b217b73fab193e464905507ef1f59d567
rdavcardoso <rogerio.davcardoso@gmail.com>
2025-06-03 11:20:05 -0300

    Criado Validador de ClienteUpdateDto.cs

---
+++ Validators/ClienteUpdateDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿namespace CrudMinimalApi.Validators;
+
+using FluentValidation;
+using CrudMinimalApi.Models.DTOs;
+
+public class ClienteUpdateDtoValidator : AbstractValidator<ClienteUpdateDto>
+{
+    public ClienteUpdateDtoValidator()
+    {
+        
+        RuleFor(x => x.Nome)
+            .NotEmpty()
+            .WithMessage("Nome é obrigatório.")
+            .Length(2, 100);
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+        
+        RuleFor(x => x.CPF)
+            .NotEmpty()
+            .WithMessage("CPF é obrigatório")
+            .Length(9)
+            .WithMessage("Apenas números.");
+    }
+    
+}
