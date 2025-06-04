45a8ec1fe0c1aa093b45e964df21a4aed7f94a53
rdavcardoso <rogerio.davcardoso@gmail.com>
2025-06-04 18:53:59 -0300

    Criado Endpoints de Produto.

--- EndPoints/ProdutoEndpoints.cs
+++ EndPoints/ProdutoEndpoints.cs
@@ -1,4 +1,6 @@
 ﻿namespace CrudMinimalApi.EndPoints;
+
+using Microsoft.EntityFrameworkCore;
 using CrudMinimalApi.Models;
 using CrudMinimalApi.Models.DTOs;
 using CrudMinimalApi.Data;
@@ -8,15 +10,61 @@ public static class ProdutoEndpoints
 {
     public static void MapProdutoEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/produtos", () => {/*Listar Produtos*/} );
+        app.MapGet("/produtos", async (AppDbContext db) =>
+        {
+            var produtos = await db.Produtos.ToListAsync();
+            
+            return Results.Ok(produtos);
+        } );
         
-        app.MapGet("/produtos/{id}", (int id) => {/*Obter Produtos por ID*/});
+        app.MapGet("/produtos/{id}", async (int id, AppDbContext db) =>
+        {
+            var produto = await db.Produtos.FindAsync(id);
+
+            return produto != null ? Results.Ok(produto) : Results.NotFound();
+        });
         
-        app.MapPost("/produtos", (ProdutoCreateDto dto) => {/*Criar Produto*/});
+        app.MapPost("/produtos", async (ProdutoCreateDto dto, AppDbContext db) =>
+        {
+            var produto = new Produto
+            {
+                Nome = dto.Nome,
+                Preco = dto.Preco,
+                Quantidade = dto.Quantidade
+            };
+
+            db.Produtos.Add(produto);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/produtos/{produto.Id}", produto);
+        });
         
-        app.MapPut("/produtos/{id}", (int id, ProdutoUpdateDto dto) => {/*Atualizar Produto*/});
+        app.MapPut("/produtos/{id}", async (int id, ProdutoUpdateDto dto, AppDbContext db) =>
+        {
+            var produto = await db.Produtos.FindAsync(id);
+            if (produto == null)
+                return Results.NotFound();
+
+            produto.Nome = dto.Nome;
+            produto.Preco = dto.Preco;
+            produto.Quantidade = dto.Quantidade;
+
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
         
-        app.MapDelete("/produtos/{id}", (int id) => {/*Deletar Produto*/});
+        app.MapDelete("/produtos/{id}", async (int id, AppDbContext db) =>
+        {
+            var produto = await db.Produtos.FindAsync(id);
+            if (produto == null)
+                return Results.NotFound();
+
+            db.Produtos.Remove(produto);
+            db.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
     }
     
 }
