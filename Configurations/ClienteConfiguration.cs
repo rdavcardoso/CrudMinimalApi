357aa599b61b922dd1d98319f3842eb60750b942
rdavcardoso <rogerio.davcardoso@gmail.com>
2025-06-17 18:02:43 -0300

    PedidoProdutoConfiguration criada.

--- Configurations/ClienteConfiguration.cs
+++ Configurations/ClienteConfiguration.cs
@@ -2,7 +2,7 @@
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using CrudMinimalApi.Models;
+using Models;
 
 public class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
 {
