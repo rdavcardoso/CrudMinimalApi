2f88464eb4142125b515f2d9266f3662e8cf2e20
rdavcardoso <rogerio.davcardoso@gmail.com>
2025-06-17 17:49:03 -0300

    ProdutoConfiguration criado.

--- Configurations/ClienteConfiguration.cs
+++ Configurations/ClienteConfiguration.cs
@@ -8,14 +8,18 @@ public class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
 {
     public void Configure(EntityTypeBuilder<Cliente> builder)
     {
+        // Nome da tabela
         builder.ToTable("Cliente");
 
+        // Chave Primária
         builder.HasKey(c => c.Id);
 
+        // Propriedade Obrigatória
         builder.Property(c => c.Nome)
             .IsRequired()
             .HasMaxLength(100);
 
+        // Relacionamento: Cliente -> Pedido (1:N)
         builder.HasMany(c => c.Pedidos)
             .WithOne(p => p.Cliente)
             .HasForeignKey(p => p.ClienteId)
