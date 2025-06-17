3f4ba1bc3d7148dbcb957a714678c4113d16a5e2
rdavcardoso <rogerio.davcardoso@gmail.com>
2025-06-17 17:36:21 -0300

    Criado ClienteConfiguration.

---
+++ Configurations/ClienteConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace CrudMinimalApi.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CrudMinimalApi.Models;
+
+public class ClienteConfiguration : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> builder)
+    {
+        builder.ToTable("Cliente");
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Nome)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasMany(c => c.Pedidos)
+            .WithOne(p => p.Cliente)
+            .HasForeignKey(p => p.ClienteId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+    
+}
