d6029bfd7648ca936b481887ad6ea9fd06eaf27a
rdavcardoso <rogerio.davcardoso@gmail.com>
2025-06-17 18:04:21 -0300

    AppDbContext atualizado para modelo IEntityTypeConfiguration retirando o modelBuilder dele.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -12,21 +12,5 @@ public class AppDbContext : DbContext
     public DbSet<Cliente> Clientes => Set<Cliente>();
     public DbSet<Pedido> Pedidos => Set<Pedido>();
     public DbSet<PedidoProduto> PedidoProdutos => Set<PedidoProduto>();
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<PedidoProduto>()
-            .HasKey(pp => new { pp.PedidoId, pp.ProdutoId });
-        
-        modelBuilder.Entity<PedidoProduto>()
-            .HasOne(pp => pp.Pedido)
-            .WithMany(p => p.Itens)
-            .HasForeignKey(pp => pp.PedidoId);
-        
-        modelBuilder.Entity<PedidoProduto>()
-            .HasOne(pp => pp.Produto)
-            .WithMany(p => p.Pedidos)
-            .HasForeignKey(pp => pp.ProdutoId);
-    }
     
 }
